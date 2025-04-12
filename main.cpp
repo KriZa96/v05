db3a53beb111a8fe081ad7bc2a8b2f76db1c9ef1
KriZa96 <kriza9605@gmail.com>
2025-04-12 08:36:20 +0200

    Rijesen labos

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
 #include <tchar.h>
 #include "nwpwin.h"
 
+#include <list>
+#include <algorithm>
+
 class main_window : public vsite::nwp::window
 {
-protected:
+  std::list<POINT> points;
+
+ protected:
 	void on_paint(HDC hdc) override  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+          if (points.empty()) return;
+          
+		  HBRUSH blueBrush = CreateSolidBrush(RGB(0, 255, 0));
+          HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, blueBrush);
+
+		  MoveToEx(hdc, points.front().x, points.front().y, nullptr);
+          std::for_each(points.begin(), points.end(),
+                        [hdc](POINT p) {
+                  ::Ellipse(hdc, p.x - 2, p.y - 2, p.x + 2, p.y + 2);
+				  ::LineTo(hdc, p.x, p.y);
+			  });
+
+		  SelectObject(hdc, oldBrush);
+          DeleteObject(blueBrush);
 	}
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+          points.push_back(p);
+          ::InvalidateRect(*this, nullptr, true);
 	}
 	void on_key_down(int vk) override
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+          if (vk == VK_ESCAPE) {
+            if (!points.empty()) {
+              points.clear();
+              ::InvalidateRect(*this, nullptr, true);
+            }
+          }
+          if (vk == VK_BACK) {
+            if (!points.empty()) {
+              points.pop_back();
+              ::InvalidateRect(*this, nullptr, true);
+            }
+          }
 	}
 	void on_destroy() override
 	{
